3c5fc662c93e522c242775ac2a12402c4da7fc9d
Arina <ahmerkulova@gmail.com>
2022-05-12 00:23:09 +0300

    add parametrization and unite all checks in one test

--- test_product_page.py
+++ test_product_page.py
@@ -1,38 +1,18 @@
+import pytest
 from .pages.product_page import ProductPage
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{offer_num}" for offer_num in range(10)]
 
 
-def test_guest_see_basket_message(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize('link', [*urls[:7], pytest.param(urls[7], marks=pytest.mark.xfail(reason="Bugged url")), *urls[8:]])
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_message_add_to_basket()
-
-
-def test_guest_see_correct_item_name(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
     page.should_be_item_name()
     page.should_be_correct_item_name_basket()
-
-
-def test_guest_see_correct_item_price(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
     page.should_be_item_price()
     page.should_be_correct_basket_price()
