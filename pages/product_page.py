643a69a78a14b5d9b3fe4bc24183f1e2ba26c461
Arina <ahmerkulova@gmail.com>
2022-05-11 17:46:08 +0300

    divide test for product page into several checks

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,9 +11,11 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_MESSAGE), \
             'Add to basket message is not presented'
 
-    def should_be_correct_item_name(self):
+    def should_be_item_name(self):
         assert self.is_element_present(*ProductPageLocators.ITEM_NAME_PRODUCT_PAGE), \
             'Item name is not presented (product page)'
+
+    def should_be_correct_item_name_basket(self):
         assert self.is_element_present(*ProductPageLocators.ITEM_NAME_BASKET), \
             'Item name is not presented (basket message)'
         item_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME_PRODUCT_PAGE).text
@@ -21,9 +23,11 @@ class ProductPage(BasePage):
         assert item_name == item_name_basket, \
             f'Item name (product page): {item_name} should be equal to item name (basket message): {item_name_basket}'
 
-    def should_be_correct_basket_price(self):
+    def should_be_item_price(self):
         assert self.is_element_present(*ProductPageLocators.ITEM_PRICE), \
             'Item price is not presented'
+
+    def should_be_correct_basket_price(self):
         assert self.is_element_present(*ProductPageLocators.BASKET_PRICE), \
             'Basket total is not presented'
         item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
--- test_product_page.py
+++ test_product_page.py
@@ -2,5 +2,37 @@ from .pages.product_page import ProductPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    pass
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+
+
+def test_guest_see_basket_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_message_add_to_basket()
+
+
+def test_guest_see_correct_item_name(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_item_name()
+    page.should_be_correct_item_name_basket()
+
+
+def test_guest_see_correct_item_price(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_item_price()
+    page.should_be_correct_basket_price()
