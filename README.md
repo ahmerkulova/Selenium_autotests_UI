ecb471009d0e73dd40aa656e424924c48d63b392
Arina <ahmerkulova@gmail.com>
2022-05-12 22:53:50 +0300

    updated base page locators

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Selenium_autotests_UI
 
-###To run negative checks:
+### To run negative checks:
 ```
 pytest -rxs -vm negative test_product_page.py
 ```
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+from .locators import BasePageLocators
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,8 +11,9 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
 
     def is_element_present(self, how, what):
         try:
@@ -34,6 +36,13 @@ class BasePage():
         except TimeoutException:
             return False
         return True
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), \
+            'Login link is not presented'
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
