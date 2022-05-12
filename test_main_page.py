fb2d10bcbc84551d4bd9f0553cf0c97acd13e05a
Arina <ahmerkulova@gmail.com>
2022-05-12 23:20:10 +0300

    change urls for main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
+link = 'http://selenium1py.pythonanywhere.com'
+login_link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
@@ -12,28 +14,24 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_should_see_correct_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+    page = LoginPage(browser, login_link)
     page.open()
     page.should_be_login_url()
 
 
 def test_guest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+    page = LoginPage(browser, login_link)
     page.open()
     page.should_be_login_form()
 
 
 def test_guest_should_see_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+    page = LoginPage(browser, login_link)
     page.open()
     page.should_be_register_form()
